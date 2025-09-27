a6087689b5d3ace6907228c88e9985199f3ef4ab
sehmaluva <machinguramalvint@gmail.com>
2025-09-27 13:28:57 +0200

    Upating  paths in the forecast app

--- apps/forecast/api/views.py
+++ apps/forecast/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from apps.forecast.models import HistoricalEntry
+from apps.forecast.models.models import HistoricalEntry
 from datetime import timedelta
 
 
