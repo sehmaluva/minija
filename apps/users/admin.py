5bbee47d89982b9637581836115cd254778b1ce8
sehmaluva <machinguramalvint@gmail.com>
2025-10-08 12:47:21 +0200

    style(users): standardize quotes and formatting in UserAdmin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,21 +2,29 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from apps.users.models.models import User
 
+
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'role', 'is_active', 'created_at')
-    list_filter = ('role', 'is_active', 'is_staff', 'created_at')
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    ordering = ('-created_at',)
-    
-    fieldsets = BaseUserAdmin.fieldsets + (
-        ('Additional Info', {
-            'fields': ('phone_number', 'role')
-        }),
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "created_at",
     )
-    
+    list_filter = ("role", "is_active", "is_staff", "created_at")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("-created_at",)
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("phone_number", "role")}),
+    )
+
     add_fieldsets = BaseUserAdmin.add_fieldsets + (
-        ('Additional Info', {
-            'fields': ('email', 'phone_number', 'role', 'first_name', 'last_name')
-        }),
+        (
+            "Additional Info",
+            {"fields": ("email", "phone_number", "role", "first_name", "last_name")},
+        ),
     )
