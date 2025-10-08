649063110628855b78d6c67d45df89ce66257b90
sehmaluva <machinguramalvint@gmail.com>
2025-10-08 12:55:37 +0200

    style(authentication): standardize quotes in CookieJWTAuthentication class

--- apps/users/api/authentication.py
+++ apps/users/api/authentication.py
@@ -1,11 +1,12 @@
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from django.conf import settings
+from rest_framework_simplejwt.authentication import JWTAuthentication  # type: ignore
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError  # type: ignore
+
 
 class CookieJWTAuthentication(JWTAuthentication):
     """
     Custom JWT authentication that looks for tokens in cookies
     """
+
     def authenticate(self, request):
         # First try to get token from Authorization header
         header = self.get_header(request)
@@ -13,7 +14,7 @@ class CookieJWTAuthentication(JWTAuthentication):
             raw_token = self.get_raw_token(header)
         else:
             # Try to get token from cookies
-            raw_token = request.COOKIES.get('access_token')
+            raw_token = request.COOKIES.get("access_token")
 
         if raw_token is None:
             return None
