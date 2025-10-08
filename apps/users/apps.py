489af556d596e5aa20b16e574218b2f2eef5121e
sehmaluva <machinguramalvint@gmail.com>
2025-10-08 12:48:08 +0200

    style(users): standardize quotes and formatting in UsersConfig class

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -2,13 +2,20 @@ from django.apps import AppConfig
 from django.contrib.admin import sites
 from django.utils.translation import gettext_lazy as _
 
+
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.users'
-    label = 'users'
-    verbose_name = _('User Management')
+    """Configuration for the Users app.
+
+    Args:
+        AppConfig (_type_): _description_
+    """
+
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.users"
+    label = "users"
+    verbose_name = _("User Management")
 
     def ready(self):
-        sites.AdminSite.site_header = _('HukuMan Administration')
-        sites.AdminSite.site_title = _('HukuMan Admin Portal')
-        sites.AdminSite.index_title = _('Welcome to HukuMan Admin')
+        sites.AdminSite.site_header = _("HukuMan Administration")
+        sites.AdminSite.site_title = _("HukuMan Admin Portal")
+        sites.AdminSite.index_title = _("Welcome to HukuMan Admin")
