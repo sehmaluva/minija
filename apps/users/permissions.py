7b823b12081aa95d6f663fe7368ca5bb827188e8
sehmaluva <machinguramalvint@gmail.com>
2025-10-08 12:48:37 +0200

    style(permissions): format permission checks for improved readability

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -1,33 +1,53 @@
 from rest_framework import permissions
 
+
 class IsOwnerOrManager(permissions.BasePermission):
     """
     Custom permission to only allow owners or managers to access certain views.
     """
-    
+
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role in ['user','admin']
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role in ["user", "admin"]
+        )
+
 
 class IsAdminOrOwner(permissions.BasePermission):
     """
     Custom permission for admin or owner access only.
     """
-    
+
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role in ['admin', 'user']
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role in ["admin", "user"]
+        )
+
 
 class CanManageBatches(permissions.BasePermission):
     """
     Custom permission for Batches management.
     """
-    
+
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role in ['user', 'admin']
-    
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role in ["user", "admin"]
+        )
+
+
 class CanViewReports(permissions.BasePermission):
     """
     Custom permission for viewing reports.
     """
-    
+
     def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.role in ['user', 'admin']
+        return (
+            request.user
+            and request.user.is_authenticated
+            and request.user.role in ["user", "admin"]
+        )
